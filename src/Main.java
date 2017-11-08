ff7ba5ea2222a022dba82f2faea85a5e7f126b7c
Michal Zietara <mzietara@gmail.com>
2017-11-08 13:34:52 +0100

    Changes 2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Siema!");
         System.out.println("Ziomeczku wirus :D");
+        System.out.println("asdadasd");
     }
 }
