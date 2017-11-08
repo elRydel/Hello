83aef1dc426ea9dd31cd31c44f6225ea3d26f597
Michal Zietara <mzietara@gmail.com>
2017-11-08 13:44:52 +0100

    wtf

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Siema!");
         System.out.println("Ziomeczku wirus :D");
-<<<<<<< HEAD
+
         System.out.println("asdadasd");
-=======
+
         System.out.println("To moje spadajjjj ahahha matjyoirentubw");
->>>>>>> e2026e9122c0914514f55da632ca539551054790
+        System.out.println("jesszcze jeden pusz Zientka");
+
     }
 }
